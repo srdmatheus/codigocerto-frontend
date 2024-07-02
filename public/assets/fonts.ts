3833ba936323cae594cb2c3ccb3e2585d8bbf0d2
Matheus Ribeiro <srdmatheus@gmail.com>
2024-07-02 11:41:47 -0300

    chore: clear project

---
+++ public/assets/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter } from "next/font/google";
+
+export const inter = Inter({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-inter"
+});
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,30 @@
 import type { Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    colors: {
+      background: {
+        DEFAULT: "rgb(var(--background))"
       },
+      foreground: {
+        DEFAULT: "rgb(var(--foreground))"
+      },
+      primary: {
+        DEFAULT: "rgb(var(--primary))"
+      }
     },
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)", ...fontFamily.sans]
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
 export default config;
