75aab03643561901b6662ef5db50be33d8693d8a
Matheus Ribeiro <srdmatheus@gmail.com>
2024-07-03 00:42:45 -0300

    feat: add hero illustration

---
+++ src/components/illustration/index.ts
@@ -0,0 +1,5 @@
+export * from "./brain-illustration";
+export * from "./gym-illustration";
+export * from "./hero-illustration";
+export * from "./people-illustration";
+export * from "./rocket-illustration";
